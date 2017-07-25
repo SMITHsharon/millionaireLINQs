c5f59a7119c15311101514a77157d157b13a03af
sharon smith <sharon.e.smith2@gmail.com>
2017-07-24 19:42:37 -0500

    split Customer and Bank classes out to .cs files

--- Program.cs
+++ Program.cs
@@ -4,18 +4,6 @@ using System.Linq;
 
 namespace Millionaires
 {
-    public class Customer
-    {
-        public string Name { get; set; }
-        public double Balance { get; set; }
-        public string Bank { get; set; }
-    }
-
-    public class Bank
-    {
-        public string Symbol { get; set; }
-        public string Name { get; set; }
-    }
 
     class Program
     {
